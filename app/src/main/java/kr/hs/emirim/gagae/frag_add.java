297b1184cfe292467919d4ee28456f8c7924f2cf
dwrdd <s2019w30@e-mirim.hs.kr>
2020-10-04 13:11:35 +0900

    화면 수정

--- app/src/main/java/kr/hs/emirim/gagae/frag_add.java
+++ app/src/main/java/kr/hs/emirim/gagae/frag_add.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Spinner;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -11,12 +12,13 @@ import androidx.fragment.app.Fragment;
 
 public class frag_add extends Fragment {
     private View view;
+    private Spinner spinner;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        view = inflater.inflate(R.layout.frag_setting,container,false);
-
+        view = inflater.inflate(R.layout.frag_add,container,false);
         return view;
+
     }
 }
