a4e841a9a7dcb1edb3873dcb69ede7e271201b5a
choiwaterrim <sxxrxm@gmail.com>
2020-11-30 05:54:33 +0900

    회원탈퇴 로그아웃 목표 금액 수정 버튼 구현

--- app/src/main/java/kr/hs/emirim/gagae/activity_setting.java
+++ app/src/main/java/kr/hs/emirim/gagae/activity_setting.java
@@ -3,12 +3,27 @@ package kr.hs.emirim.gagae;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
 public class activity_setting extends AppCompatActivity {
+
+    private EditText st_limitmoney;
+    private Button btn_logout, btn_signout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_setting);
+
+        st_limitmoney = findViewById(R.id.st_limitmoney);
+        btn_logout = findViewById(R.id.btn_logout);
+        btn_signout = findViewById(R.id.btn_signout);
+
+        b
+
+
     }
 }
