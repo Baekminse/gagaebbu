b2ad7af1fe9a42c4e7271f4fe4f25749b189b097
choiwaterrim <sxxrxm@gmail.com>
2020-11-30 18:10:28 +0900

    기능 구현 완료

--- app/src/main/java/kr/hs/emirim/gagae/activity_add.java
+++ app/src/main/java/kr/hs/emirim/gagae/activity_add.java
@@ -10,13 +10,19 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
 
 public class activity_add extends AppCompatActivity {
@@ -30,12 +36,16 @@ public class activity_add extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add);
+        Calendar cal = Calendar.getInstance();
 
         btn_commit = findViewById(R.id.btn_commit);
         usemoney = findViewById(R.id.usemoney);
 
         FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
         final String uid = user.getUid();
+        SimpleDateFormat format = new SimpleDateFormat("yyyy-MM/dd");
+        final String date = format.format(Calendar.getInstance().getTime());
+
 
         databaseReference = databaseReference.child(uid);
 
@@ -43,10 +53,22 @@ public class activity_add extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 if(usemoney != null){
-                    Date date = new Date(System.currentTimeMillis()); //날짜
-                    databaseReference.child("가계부").child(date.toString()).child("소비 금액").setValue(usemoney.getText().toString());
-                    startActivity(new Intent(activity_add.this, activity_home.class));
-                    finish();
+                    databaseReference.child("가계부").child(date).child("소비 금액");
+                    databaseReference.addValueEventListener(new ValueEventListener() {
+                        @Override
+                        public void onDataChange(@NonNull DataSnapshot snapshot) {
+                            databaseReference.child("가계부").child(date).child("소비 금액").setValue(usemoney.getText().toString());
+                            startActivity(new Intent(activity_add.this, activity_home.class));
+                            finish();
+                        }
+
+                        @Override
+                        public void onCancelled(@NonNull DatabaseError error) {
+
+                        }
+                    });
+
+
                 }else{
                     Toast.makeText(getApplicationContext(),"값이 없습니다.", Toast.LENGTH_SHORT).show();
                 }
--- app/src/main/java/kr/hs/emirim/gagae/activity_home.java
+++ app/src/main/java/kr/hs/emirim/gagae/activity_home.java
@@ -1,12 +1,15 @@
 package kr.hs.emirim.gagae;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.*;
 
 import com.google.firebase.auth.FirebaseAuth;
@@ -18,18 +21,19 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
 public class activity_home extends AppCompatActivity {
-    private TextView limitmoney;
-    private TextView uName;
+    private TextView limitmoney, diaryTextView;
+    private TextView uName, show_money;
     private ImageButton btn_add, btn_setting;
+    private CalendarView calendarView;
     private FirebaseDatabase firebaseDatabase = FirebaseDatabase.getInstance();
     private DatabaseReference databaseReference = firebaseDatabase.getReference("Users");
     private DatabaseReference mReference = firebaseDatabase.getReference("Users");
+    private DatabaseReference dReference = firebaseDatabase.getReference("Users");
     private long time = 0;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
@@ -42,10 +46,10 @@ public class activity_home extends AppCompatActivity {
         uName = findViewById(R.id.username);
         btn_setting = findViewById(R.id.btn_setting);
         btn_add = findViewById(R.id.btn_add);
+        show_money = findViewById(R.id.show_money);
+        diaryTextView=findViewById(R.id.diaryTextView);
         getfirebase(uid);
         getlimitmoney(uid);
-
-
 
         btn_add.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -60,7 +64,36 @@ public class activity_home extends AppCompatActivity {
                 startActivity(new Intent(activity_home.this, activity_setting.class));
             }
         });
+        calendarView = findViewById(R.id.calendarView);
+        calendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+            @Override
+            public void onSelectedDayChange(@NonNull CalendarView view, int year, int month, int dayOfMonth) {
+                diaryTextView.setVisibility(View.VISIBLE);
+                diaryTextView.setText(String.format("%d / %d / %d" ,year ,month+1 ,dayOfMonth));
+                String Syear = String.valueOf(year);
+                String Smonth = String.valueOf(month+1);
+                String yemo = Syear+"-"+Smonth;
+                dReference = dReference.child(uid).child("가계부").child(yemo).child(String.valueOf(dayOfMonth)).child("소비 금액");
+                dReference.addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                        if (dataSnapshot.exists()){
+                            String here = dataSnapshot.getValue().toString();
+                            show_money.setText(here);
+                        }else {
+                            show_money.setText("기록된 소비가 없어요");
+                        }
 
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError error) {
+
+                    }
+                });
+
+            }
+        });
 
     }
 
@@ -110,5 +143,6 @@ public class activity_home extends AppCompatActivity {
             finish();
         }
     }
+
 
 }
